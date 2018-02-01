e710c2b1981061f69144c5968df599a75d84af6d
Jan Sipr <sipr.jan@gmail.com>
2018-02-01 15:13:00 +0100

    Extract service effect and generalize it

---
+++ src/Control/Monad/Freer/Service.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Control.Monad.Freer.Service
+    ( IscCall(..)
+    , ServiceChannel
+    , createServiceChannel
+    , runServiceChannel
+    , runServiceEffect
+    , interpret
+    )
+  where
+
+import Control.Applicative ((<$>), pure)
+import Control.Concurrent.MVar (MVar, newEmptyMVar, takeMVar, putMVar)
+import Control.Concurrent.STM.TChan (TChan, newTChanIO, writeTChan, readTChan)
+import Control.Monad ((>>=))
+import Control.Monad.Freer (Eff, Member, send, handleRelay)
+import Control.Monad.STM (atomically)
+import Data.Function (($))
+import System.IO (IO)
+
+
+class IscCall service where
+    data ChannelData service :: * -> *
+    get :: ChannelData service a -> service a
+    put :: service a -> ChannelData service a
+
+data ChannelContainer service where
+    ChannelContainer
+        :: forall a service. IscCall service
+        => ChannelData service a
+        -> MVar a
+        -> ChannelContainer service
+
+data ServiceChannel service where
+    ServiceChannel
+        :: IscCall service
+        => TChan (ChannelContainer service)
+        -> ServiceChannel service
+
+createServiceChannel :: IscCall service => IO (ServiceChannel service)
+createServiceChannel = ServiceChannel <$> newTChanIO
+
+runServiceChannel
+    :: IscCall service
+    => ServiceChannel service
+    -> (forall a. (a -> IO ()) -> service a -> IO ())
+    -> IO ()
+runServiceChannel schan@(ServiceChannel chan) f = do
+    val <- atomically $ readTChan chan
+    case val of
+        ChannelContainer v resMVar -> f (putMVar resMVar) $ get v
+    runServiceChannel schan f
+
+runServiceEffect
+    :: forall effs service a
+    . (Member IO effs, IscCall service)
+    => ServiceChannel service
+    -> Eff (service ': effs) a
+    -> Eff effs a
+runServiceEffect (ServiceChannel chan) = interpret go
+  where
+    go :: service s -> Eff effs s
+    go v = send $ do
+        respMVar <- newEmptyMVar
+        atomically $ writeTChan chan (ChannelContainer (put v) respMVar)
+        takeMVar respMVar
+
+-- TODO: move this into freer-effects
+interpret :: (forall s. eff s -> Eff effs s) -> Eff (eff ': effs) a -> Eff effs a
+interpret f = handleRelay pure (\v f' -> f v >>= f')
--- src/Database/Service.hs
+++ src/Database/Service.hs
@@ -1,119 +1,63 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LambdaCase #-}
---{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Database.Service
-    ()
   where
 
-import Control.Applicative (pure)
-import Control.Concurrent.MVar (MVar, newEmptyMVar, takeMVar, putMVar)
-import Control.Concurrent.STM.TChan (TChan, writeTChan, readTChan)
 import Control.Monad ((>>=), (>>))
-import Control.Monad.Freer (Eff, Member, send, handleRelay)
-import Control.Monad.STM (atomically)
-import Data.Acid (AcidState, Query, Update, makeAcidic, query, update)
+import Control.Monad.Freer (Eff, Member)
+import Data.Acid (AcidState)
 import Data.Function (($))
 import Data.Vector (Vector)
 import System.IO (IO)
 
-import Database.Model (DataModel, Feed, SetFeed, listFeeds, defaultDataModel, addFeedIfUnique)
+import Database.Model (DataModel, Feed, SetFeed, listFeeds, addFeedIfUnique)
+import Control.Monad.Freer.Service
+    ( IscCall(ChannelData, get, put)
+    , ServiceChannel
+    , createServiceChannel
+    , runServiceChannel
+    , runServiceEffect
+    )
 
-
-class IscCalls service where
-    data ChannelData service :: * -> *
-
---data ServiceConnection service where
---    ServiceConnection
---        :: Isc service
---        => TChan (ChannelData service a, MVar a)
---        -> ServiceConnection service
-
---class IscConnection service where
---    data ServiceConnection service :: *
---
---instance Isc service => IscConnection service where
---    data ServiceConnection service = forall a. TChan (ChannelData service a, MVar a)
-
--- data ServiceConnection service = forall a. ServiceConnection (TChan (ChannelData service a, MVar a))
-
---class (Isc service) => IscTChan service where
---    runService
---        :: ServiceConnection service
---        -> ChannelData service a -> (a -> IO ()) -> IO b
---        -> IO b
---
---    runServiceEffect
---        :: forall eff effs a. Member IO effs
---        => ServiceConnection service
---        -> Eff (ChannelData service ': effs) a
---        -> Eff effs a
---    runServiceEffect (ServiceConnection chan) = interpret go
---      where
---        go v = send $ do
---            respMVar <- newEmptyMVar
---            atomically $ writeTChan (chan :: TChan (ChannelData service a, MVar a)) (v, respMVar)
---            takeMVar respMVar
-
---class (IscCalls service) => IscService service where
---    data ServiceConnection service :: *
-
-
-
-undef :: a
-undef = undef
-
--- instance (IscCalls service) => IscService service where
-    -- data ServiceConnection service = TChan (ChannelData service a, MVar a)
-runService
-    :: DatabaseConnection
-    -> (forall a. (a -> IO ()) -> Database a -> IO b)
-    -> IO b
-runService chan f = do
-    val <- atomically $ readTChan chan
-    case val of
-        Dd (v, resMVar)-> f (putMVar resMVar) v
-    runService chan f
-
-runServiceEffect
-    :: forall eff effs a s. Member IO effs
-    => DatabaseConnection
-    -> Eff (Database ': effs) a
-    -> Eff effs a
-runServiceEffect chan = interpret go
-  where
-    go :: forall s effs. Member IO effs => Database s -> Eff effs s
-    go v = send $ do
-        respMVar <- newEmptyMVar
-        atomically $ writeTChan chan (Dd (v, respMVar))
-        takeMVar respMVar
-
-data Dd = forall a. Dd (Database a, MVar a)
-type DatabaseConnection = TChan Dd
 
 data Database s where
     AddFeedIfUnique :: SetFeed -> Database ()
     ListFeeds :: Database (Vector Feed)
 
-runDatabase :: AcidState DataModel -> (a -> IO ()) -> Database a -> IO ()
-runDatabase state return' = \case
+instance IscCall Database where
+    data ChannelData Database a = WrapDatabase (Database a)
+
+    get :: ChannelData Database a -> Database a
+    get (WrapDatabase v) = v
+
+    put :: Database a -> ChannelData Database a
+    put = WrapDatabase
+
+processDatabase :: AcidState DataModel -> (a -> IO ()) -> Database a -> IO ()
+processDatabase state return' = \case
     AddFeedIfUnique v -> addFeedIfUnique state v >> return' ()
     ListFeeds -> listFeeds state >>= return'
---syncCall :: (eff s -> IO a) -> IO a
---syncCall =
 
-interpret :: (forall s. eff s -> Eff effs s) -> Eff (eff ': effs) a -> Eff effs a
-interpret f = handleRelay pure (\v f' -> f v >>= f')
+createDatabaseChannel :: IO (ServiceChannel Database)
+createDatabaseChannel = createServiceChannel
 
---runDatabase :: Member IO r => TChan (Database s) -> Eff (Database ': r) w -> Eff r w
---runDatabase = interpret go
+runDatabase :: AcidState DataModel -> ServiceChannel Database -> IO ()
+runDatabase model chan = runServiceChannel chan $ processDatabase model
+
+runDatabaseEffect
+    :: Member IO effs => ServiceChannel Database
+    -> Eff (Database ': effs) a
+    -> Eff effs a
+runDatabaseEffect = runServiceEffect
