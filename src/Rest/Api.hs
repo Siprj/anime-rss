01bbf2bd5cc09ff884d18709c3c99cbed7728c6a
Jan Sipr <sipr.jan@gmail.com>
2020-08-15 15:46:42 +0200

    More API

--- src/Rest/Api.hs
+++ src/Rest/Api.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
@@ -8,7 +10,9 @@
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Rest.Api
     ( Api
@@ -17,18 +21,21 @@ module Rest.Api
     )
   where
 
-import GHC.Generics (Generic)
+import Core.Type.Id (UserId, AnimeId)
+import Core.Type.User (Email)
+import Data.Eq (Eq)
+import Data.Text (Text)
+import Data.Time (UTCTime)
 import Data.UUID (UUID)
+import GHC.Generics (Generic)
+import Network.URI (URI)
+import Optics (makeFieldLabelsWith, noPrefixFieldLabels)
+import Rest.AtomMime (AtomFeed)
 import Servant.API (ReqBody, PostNoContent, (:<|>), (:>), Get, JSON)
 import Servant.Auth.Server (Auth, JWT)
 import Text.Feed.Types (Feed)
-import Core.Type.Id (UserId)
-import Core.Type.User (Email)
-
-import Rest.AtomMime (AtomFeed)
 import Text.Show (Show)
-import Data.Eq (Eq)
-import Data.Text (Text)
+import Data.Bool (Bool)
 
 
 type ChannelId = UUID
@@ -42,19 +49,49 @@ data User = User
     }
   deriving (Show, Eq, Generic)
 
+makeFieldLabelsWith noPrefixFieldLabels ''User
+
 data Login = Login
     { email :: Email
     , password :: Text
     }
+
+makeFieldLabelsWith noPrefixFieldLabels ''Login
 
 data UserSession = UserSession
     { userId :: UserId
     }
   deriving (Show, Eq, Generic)
 
+makeFieldLabelsWith noPrefixFieldLabels ''UserSession
+
+data Anime = Anime
+    { animeId :: AnimeId
+    , title :: Text
+    , url :: URI
+    , imageUrl :: URI
+    , date :: UTCTime
+    }
+  deriving (Show, Eq, Generic)
+
+makeFieldLabelsWith noPrefixFieldLabels ''Anime
+
+data PostAnimeFollow = PostAnimeFollow
+    { animeId :: AnimeId
+    , follow :: Bool
+    }
+  deriving (Show, Eq, Generic)
+
+makeFieldLabelsWith noPrefixFieldLabels ''PostAnimeFollow
+
+type Protected
+    = "user" :> Get '[JSON] User
+    :<|> "anime" :> ReqBody '[JSON] [PostAnimeFollow] :> PostNoContent
+
 type Api
     = "atom" :> Get '[AtomFeed] Feed
     :<|> "atom" :> "episodes" :> ChannelId :> Get '[AtomFeed] Feed
     :<|> "atom" :> "anime" :> ChannelId :> Get '[AtomFeed] Feed
     :<|> "login" :> ReqBody '[JSON] Login :> PostNoContent
-    :<|> (Servant.Auth.Server.Auth '[JWT] UserSession :<|> "user" :> Get '[JSON] User)
+    :<|> "anime" :> Get '[JSON] [Anime]
+    :<|> Servant.Auth.Server.Auth '[JWT] UserSession :> Protected
